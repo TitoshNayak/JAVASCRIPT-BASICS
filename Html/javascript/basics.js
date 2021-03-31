924100d4aa1e5b77115334ad1392e5cca445d975
unknown <nayaktitosh@gmail.com>
2021-03-31 08:53:54 +0530

    2

--- Html/javascript/basics.js
+++ Html/javascript/basics.js
@@ -1,5 +1,45 @@
-var x=9;
-document.write(x)
-             
-//hell0
-//
+<html>
+    <head>
+        <title>My first JavaScript program</title>
+     
+    </head>
+
+
+    <script type="text/javascript">
+        var var1=120;
+        var var2=true;
+        var var3=new Date();
+        var var4="Nayak";
+        var var5=new String("Bhagya");
+        var var6;
+        document.write("<br/> The types of var1="+typeof(var1);
+        document.write("<br/> The types of var2="+typeof(var2);
+        document.write("<br/> The types of var3="+typeof(var3);
+        document.write("<br/> The types of var4="+typeof(var4);
+        document.write("<br/> The types of var5=+typeof(var5);
+        document.write("<br/> The types of var6=+typeof(var6);
+
+    </script>
+</html>
+
+
+       //Javascript comments
+//This is the single line comment 
+/*
+  It is a multi line comments
+  *Group of code are not executed inside the multi line comments. 
+   <script type="script/javascript">
+          document.write("<h1 ><i>Welcome to javascript</i></h1>");
+      </script>
+     
+*/
+
+ /*Javascript Datatypes:
+
+
+1.Numbers
+2.Booleans
+3.Null
+4.String
+5.Undefined
+*/
