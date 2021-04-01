b88737eea3f35080e93e7827a2b8085007daac8a
TitoshNayak <nayaktitosh@gmail.com>
2021-04-01 09:13:59 +0530

    hi

---
+++ smallprogram.js
@@ -0,0 +1,44 @@
+//function with no return value
+
+/*function Age(year)
+{
+    console.log(2010 - year);
+}
+Age(2019);
+*/
+function Age(year)
+{
+    return( 20-year);
+    console.log(age);
+}
+Age(2019);
+
+
+     //Array concept in javascript
+    //write a program to find the sum of the value using for loop
+
+    let num=[1,2,3,4,5,10]
+    let sum=0;
+    for(let i=0;i<=num.length -1;i++)
+    {
+        sum=sum+num[i];
+    }
+    console.log(sum);
+
+    //execute a program using while loop
+
+    let num=[1,2,3,4,5,10];
+    let i=0;
+    let sum=0;
+    while(i<=num.lenght -1)
+    {
+        sum=sum + num[i];
+        i++;
+    }
+    console.log(sum);
+  
+
+    // program to write a key values
+
+ 
+ 
