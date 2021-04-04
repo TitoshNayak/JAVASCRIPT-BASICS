ee28fc3465b3ce145101d8f7814d845b16b3fca4
TitoshNayak <nayaktitosh@gmail.com>
2021-04-04 09:20:03 +0530

    assignment1

---
+++ Html/javascript/Arrays/Fatarrow.js
@@ -0,0 +1,67 @@
+// Fat Arrays in javascript
+/*
+var wish =()=>
+{
+    console.log("Good Morning");
+};
+wish();
+
+var wish =()=>
+{
+    console.log("Good evening");
+};
+wish();
+*/
+/*
+//Advantages of the fat array are:
+1.write the minimal coding  or reeduce the code length of the PerformanceNavigationTiming.
+2. if the function has one statement than can use the 
+     implicite return
+     example: 
+            let add=()=>
+            {
+                console.log("Addition");
+            };
+            add();
+            */ 
+    /*//function return value using fat arrays in javascript
+    let add=(a,b)=>
+{
+    return a+b;
+}
+   let result=add(5,6);
+   console.log(result);
+
+*/
+
+// fat array wit function parameter
+
+//syntax:
+
+/*let add=()=>{};
+
+add();
+
+*/
+
+
+//example:
+ /* let add=(a)=>
+  {
+      console.log(a+5);
+  };
+  add(10);
+  add(29);
+*/
+
+//Function return value using fat array
+//program:
+  let message=()=>
+  {
+      return ("I love web developement");
+  };
+
+  let msg=message("hello");
+  console.log(msg);
+
+ 
--- Html/javascript/Assignment1.js
+++ Html/javascript/Assignment1.js
@@ -1,46 +1,56 @@
 /*
-//1.write a program to print 1 to 50 number using while and for loop
-
-for(var i=1;i <=50; i++)
+//1.write a program to print 1 to 10 number using while and for loop
+var i=1;
+for(var i=1;i <=10; i++)
 {
     console.log(i);
 }
 
 //while
-while(i<=50)
+var i=0
+while(i<=10)
 {
+
+    i++;
     console.log(i);
 }
 
 
+
 //2.print 10 to 1 number
-for(var i=10; i <=10; i--)
+var i=10;
+for(var i=10; i >=1; i--)
 {
     console.log(i);
 
 }
 
 //while loop using
-
-while(var i=10; i<=10; i--)
+var i=10;
+while(i>=1)
 {
+     i--;
     console.log(i);
 }
 
 
-//3. print the odd number less than 100
+//3. print the odd number less than 50
+var i=1;
 
-for(var i=1; i<=100; i +=2)
+for(var i=1; i<=50; i +=2)
 {
     console.log(i);
 }
   
-// while loop
 
-while(var i=1; i<=100; i +=2)
+// while loop
+var i=1;
+while(i<30)
 {
+    i+=2;
     console.log(i);
 }
+/*
 
 //4. print the multiplication table with 8
 
@@ -52,38 +62,44 @@ for( var i=1; i<=8; i++)
 }
 
 //while loop
-
-while(var i=1;i<=8;1++)
+var i=1;
+while(i<=8)
 {
+    i++;
     var row="8*"+i+"="+8*i;
     console.log(row);
 }
 
-// 5.print number 5 to 15 using the loops concept
 
-for(var i=5; i <=15; i++)
+// 5.print number 5 to 15 using the loops concept
+var i=5;
+for( i=5; i <=15; i++)
 {
     console.log(i);
 }
 
 //while
-while(var i=5; i<=15;i++)
+var i=5;
+while(i<15)
 {
+    i++;
     console.log(i);
 }
+
 
 // 6.print number 15 to 10 using the loops concept
-
-for(var i=10; i <=15; i--)
+var i=15;
+for(var i=15; i >=10; i--)
 {
     console.log(i);
 }
 
 //while
-
-while(var i=10; i<=15;i--)
-
+var i=15;
+while (i>10)
+   
 {
+    i--;
     console.log(i);
 }
 
@@ -103,14 +119,14 @@ for(i=1;i<=10;i++)
 var i=0;
  while(i<=10)
  {
-     i +=2;
+    i +=2;
     console.log(i);
     
  }
  
 //8. print 1 to 10 odd number
-
-
+ //for loop
+var i=1;
 for(i=1;i<=10;i++)
 {
     if(i % 2 !==0)
@@ -118,8 +134,8 @@ for(i=1;i<=10;i++)
         console.log(i);
     }
 }
-*/
-// odd number
+
+// odd number print out 1 to 10
 var i=1;
  while(i<=10)
  {
@@ -129,14 +145,98 @@ var i=1;
  }
 
 
+//7.1 write a program first ten multiples of 4 using a while loop
+
+var i=1;
+while(i<=10)
+{
+   i++;
+    var row="4*"+i+"="+4*i;
+    
+    console.log(row);
+}
+
+var i=1;
+for( var i=1;i<=10;i++)
+{
+    var row="4*"+i+"="+4*i;
+    console.log(row);
+}
+
+//8. write a program multiples of 4  are 4,8,12,16 using a while loop
+
+//while loop
+var i=0;
+while(i<=3)
+{
+   i++;
+    var row="4*"+i+"="+4*i;
+    
+    console.log(row);
+}
+
+//9. write a program to print multiplication table of 6 using while loop
+//while loop
+var i=0;
+while(i<=20)
+{
+   i++;
+    var row="6*"+i+"="+6*i;
+    
+    console.log(row);
+}
+
+*/
+/*//10. write factorial of 24!
+var i=1;
+var fact=1;
+while(i<=5)
+{
+    i--;
+  var fact="5*"+i+"="+5*i;
+   console.log(fact);
+}
 
 
+ // program to find the factors of an integer
 
+// take input
+/*const num = prompt('Enter a positive number: ');
 
+console.log(`The factors of ${num} is:`);
 
+// looping through 1 to num
+for(let i = 1; i <= num; i++) {
 
+    // check if number is a factor
+    if(num % i == 0) {
+        console.log(i);
+    }
+}
+*/
+//10. factor of the numbers
 
+var number=prompt("Please Enter the number");
 
+for(var i=1;i<=number;i++)
+{
+    if(number % i == 0)
+    {
+        console.log(i);
+    }
+}
+
+//factors of the nmbber using while loop
+/*
+var number=prompt("Please Enter the number");
+while( i<=number);
+{
+      i++;
+    if(number % == 0)
+    {
+       console.log(i);
+   }
+}*/
 
 
 
