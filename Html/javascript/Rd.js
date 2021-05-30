4d412e5e9d33a21cf00ccd1af39a92720108338a
TitoshNayak <nayaktitosh@gmail.com>
2021-05-30 21:37:08 +0530

    alert

--- Html/javascript/Rd.js
+++ Html/javascript/Rd.js
@@ -4,8 +4,24 @@
 //console.log(y);
 
 //extract and copy the array values
-
+/*
 let values = [2, 4, 6, 7];
 values.map((x) => {
   console.log(x);
 });
+*/
+/*
+function add(a,b){
+  return a/b;
+}
+let result =add(5,9);
+console.log(result);
+
+
+let sum=(a,b)=>{
+  console.log(a+b);
+}
+sum(5,19);
+*/
+let add=(a,b)=>"hello";
+console.log(add());
