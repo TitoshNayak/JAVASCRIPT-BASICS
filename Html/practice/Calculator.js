07437e06b5058cdf4aa26908d4eecfb1e6150012
TitoshNayak <nayaktitosh@gmail.com>
2021-10-25 15:31:40 +0530

    chande added

---
+++ Html/practice/Calculator.js
@@ -0,0 +1,13 @@
+
+
+function Add(a,b){
+    return (a+b);
+
+}
+ let Result =Add(5,8);
+ console.log(Result);
+
+ 
+
+ //let Add =(a, b)=>a+b;
+ 
---
+++ Html/practice/practice.js
@@ -0,0 +1,8 @@
+
+let num=2;
+if(num % 3==0){
+    console.log("Dividible by 3" +num);
+}
+else{
+    console.log("It is no divisible by 3" +num);
+}
